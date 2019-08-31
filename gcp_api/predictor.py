42d57d463f3469013e8815c4a0db18b0da532452
Trent Woodbury <TrentWoodbury@gmail.com>
2019-08-31 18:00:47 +0200

    Add google cloud integration files. Still not verified running.

---
+++ gcp_api/predictor.py
@@ -0,0 +1,54 @@
+import os
+import pickle
+
+import numpy as np
+from xgboost import XGBClassifier
+
+
+class MyPredictor(object):
+    """An example Predictor for an AI Platform custom prediction routine."""
+
+
+    def __init__(self, model):
+        """Stores artifacts for prediction. Only initialized via `from_path`.
+        """
+        self._model = model
+
+
+    def predict(self, instances, **kwargs):
+        """
+        Performs custom prediction. Predicts the probability of a default for
+        the given instances.
+
+        Args:
+            instances: A list of prediction input instances.
+            **kwargs: A dictionary of keyword args provided as additional
+                fields on the predict request body.
+
+        Returns:
+            A list of outputs containing the prediction results.
+        """
+        inputs = np.asarray(instances)
+        outputs = self._model.predict_proba(preprocessed_inputs)[:, 1]
+        return outputs.tolist()
+
+
+    @classmethod
+    def from_path(cls, model_dir):
+        """Creates an instance of MyPredictor using the given path.
+
+        This loads artifacts that have been copied from your model directory in
+        Cloud Storage. MyPredictor uses them during prediction.
+
+        Args:
+            model_dir: The local directory that contains the trained
+                xgboost model.
+
+        Returns:
+            An instance of `MyPredictor`.
+        """
+        model_path = os.path.join(model_dir, 'model.pkl')
+        with open(model_path, 'rb') as f:
+            model = pickle.load(f)
+
+        return cls(model)
---
+++ gcp_api/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+    name='custom_prediction_script',
+    version='0.1',
+    scripts='predictor.py')
